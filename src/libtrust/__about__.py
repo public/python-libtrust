b3396e3bab9529d08a5e533dabe814729a848cbb
Alex Stapleton <alexs@prol.etari.at>
2014-12-11 09:57:50 +0000

    Missing __about__.py

---
+++ src/libtrust/__about__.py
@@ -0,0 +1,22 @@
+# This file is dual licensed under the terms of the Apache License, Version
+# 2.0. See the LICENSE file in the root of this repository for complete
+# details.
+
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "libtrust"
+__summary__ = "Docker's libtrust but in Python."
+__uri__ = "https://github.com/public/libtrust"
+
+__version__ = "0.0.1"
+
+__author__ = "Alex Stapleton"
+__email__ = "alexs@prol.etari.at"
+
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "Copyright 2013-2014 {0}".format(__author__)
